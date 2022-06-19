0a96d6576ae906986c7be24a3491d79d96e40f14
Leonid Nazarov <work.leonid@gmail.com>
2022-06-19 17:30:37 +0300

    add year column to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_18_114539) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_19_143011) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,6 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_18_114539) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "subtitle"
+    t.integer "year"
   end
 
   create_table "courses", force: :cascade do |t|

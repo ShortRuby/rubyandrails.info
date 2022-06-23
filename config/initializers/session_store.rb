61dc53ee1bb92335a265dc8126b79f100eebe232
Leonid Nazarov <work.leonid@gmail.com>
2022-06-24 00:23:50 +0500

    change cookie key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_damp-journey-23406', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_rubyandrails_session', domain: :all, tld_length: 2

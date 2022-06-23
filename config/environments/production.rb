7f0963376e8012ff1020af6745922c17d854e527
Leonid Nazarov <work.leonid@gmail.com>
2022-06-23 23:32:29 +0500

    config cookie

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
+  config.action_dispatch.cookies_same_site_protection = :None
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 

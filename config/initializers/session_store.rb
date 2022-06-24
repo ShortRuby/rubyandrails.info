5a6713b25cabbffa04ca6672f3edf2fb83383e26
Leonid Nazarov <work.leonid@gmail.com>
2022-06-24 10:09:05 +0500

    fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 Rails.application.config.session_store :cookie_store, 
   key: '_rubyandrails_session', 
   domain: :all, 
-  same_site: :none,
+  same_site: :strict,
   secure: :true,
   tld_length: 2

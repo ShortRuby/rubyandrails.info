bdf1726cc85a4900a00f657cb6f13f5e95aa9d1a
Leonid Nazarov <work.leonid@gmail.com>
2022-06-24 10:36:31 +0500

    fix

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module RubyandrailsInfo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.action_dispatch.cookies_same_site_protection = :strict
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,6 @@
 Rails.application.config.session_store :cookie_store, 
   key: '_rubyandrails_session', 
   domain: :all, 
+  same_site: :none,
+  secure: :true,
   tld_length: 2

84d31bbb421ec0fa4ba54b091ae791a477e4453c
Leonid Nazarov <work.leonid@gmail.com>
2022-06-24 09:46:13 +0500

    trying

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_rubyandrails_session', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_rubyandrails_session', same_site: :none, secure: :true, domain: :all, tld_length: 2

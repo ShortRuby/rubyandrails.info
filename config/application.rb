7b7bb40f17c29303fd078f36befb8919d1cc968c
Leonid Nazarov <work.leonid@gmail.com>
2022-06-24 10:15:18 +0500

    fix

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RubyandrailsInfo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.action_dispatch.cookies_same_site_protection = :lax
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

1e0c238a9371d5bd733bfbab168d4fb51de6d350
Leonid Nazarov <work.leonid@gmail.com>
2022-06-25 08:34:43 +0500

    add middleware for cloudflare

--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,19 @@ require_relative "boot"
 
 require "rails/all"
 
+require_relative "cloudflare_proxy"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 
 module RubyandrailsInfo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.use CloudflareProxy
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
---
+++ config/cloudflare_proxy.rb
@@ -0,0 +1,15 @@
+# https://github.com/rails/rails/issues/22965
+require "json"
+
+class CloudflareProxy
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    return @app.call(env) unless env["HTTP_CF_VISITOR"]
+
+    env["HTTP_X_FORWARDED_PROTO"] = JSON.parse(env["HTTP_CF_VISITOR"])["scheme"]
+    @app.call(env)
+  end
+end

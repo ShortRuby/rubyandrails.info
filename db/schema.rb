55a4b14b36009d638fcf9a62cd3187b37cdad2a7
Leonid Nazarov <work.leonid@gmail.com>
2022-07-02 11:15:01 +0500

    update all

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_19_192904) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_19_202542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_19_192904) do
     t.string "siteUrl"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_authors_on_slug", unique: true
   end
 
   create_table "books", force: :cascade do |t|
@@ -124,6 +126,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_19_192904) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_tags_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|

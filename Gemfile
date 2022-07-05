a51aca8b23d05b7020b1831cf4c38a6964dd5298
Leonid Nazarov <work.leonid@gmail.com>
2022-07-05 17:19:12 +0500

    add faker

--- Gemfile
+++ Gemfile
@@ -62,6 +62,10 @@ gem 'acts-as-taggable-on', '~> 9.0'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+
+  # https://github.com/faker-ruby/faker
+  gem 'faker'
+
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+
+require 'faker'
+
+
+20.times do |tag|
+  Tag.create(title: Faker::Lorem.word)
+end
+
+100.times do |book|
+  Book.create!(title: Faker::Book.title[0..40], 
+               subtitle: Faker::Book.title[0..20], 
+               content: Faker::Lorem::paragraph, 
+               year: Faker::Number.between(from: 2000, to: 2022),
+               tag_ids: [] )
+end

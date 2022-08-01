62701acfda5f3006ec7f6c79e87673530f83f169
Leonid Nazarov <work.leonid@gmail.com>
2022-08-01 16:58:08 +0300

    update seo config

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,10 @@ Rails.application.routes.draw do
 
 #  root "landing#index"
    root "books#index" 
+
+  constraints(host: /^(?!www\.)/i) do
+    get '(*any)' => redirect { |_params, request|
+      URI.parse(request.url).tap { |uri| uri.host = "www.#{uri.host}" }.to_s
+    }
+  end
 end

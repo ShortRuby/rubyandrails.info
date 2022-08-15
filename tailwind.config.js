5d48911ef644d0deb81b2b258681a546cf93fbf4
Leonid Nazarov <work.leonid@gmail.com>
2022-08-15 18:23:11 +0300

    remove files and fix viewcomponents path to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,12 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+	'./app/components/**/*.html.erb',
+    './app/components/**/*.rb',
   ],
   theme: {
     extend: {
-	  backgroundImage: {
-		'bg': "url('bg.jpg')"
-	  },
-	  fontFamily: {
-		'custom': ['Syne', 'sans-serif']
-	  }
 	},
   },
   plugins: [

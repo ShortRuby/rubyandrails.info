0ba2ef2a09c52f97323206130d6c2dbba13faf13
Leonid Nazarov <work.leonid@gmail.com>
2022-08-16 13:27:46 +0300

    add og tags and cover

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'rubyandrails.info',
+      keywords: 'ruby, books, podcasts, newsleters, free books, testing, hotwire, for juniors',
+      og: {
+        site_name: 'rubyandrails.info',
+        title: 'Discover Ruby and Ruby on Rails materials',
+        description: 'Discover free and paid books, newsletters, podcasts about Ruby & Ruby on Rails in one place.', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('og_cover.jpg')
+      }
+    }
+  end
 end

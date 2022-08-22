d7784f86c31f72131d376c44e72cff9f2c638614
Leonid Nazarov <work.leonid@gmail.com>
2022-08-22 18:31:25 +0300

    update book design

---
+++ app/components/section/featured_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Section::FeaturedComponent < ViewComponent::Base
+
+end
---
+++ test/components/section/featured_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Section::FeaturedComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Section::FeaturedComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end

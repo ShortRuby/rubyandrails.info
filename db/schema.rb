a179ea13da6d2996795aacce2d267014f27123ea
David Young <davie668@gmail.com>
2023-03-22 15:15:12 +0000

    Added mastodon_url to Authors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_091734) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_151355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_091734) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.string "photo"
+    t.string "mastodon_url"
     t.index ["slug"], name: "index_authors_on_slug", unique: true
   end
 

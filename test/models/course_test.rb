92b7d5d20e7d31e2d27789b166f64f5c985bca28
Lucian Ghinda <lucianghinda@users.noreply.github.com>
2023-03-24 08:16:47 +0200

    Fixed cover for courses

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,22 +1,36 @@
-# == Schema Information
-#
-# Table name: courses
-#
-#  id              :bigint           not null, primary key
-#  title           :string
-#  content         :text
-#  free            :boolean
-#  courseSiteTitle :string
-#  courseSiteUrl   :string
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  slug            :string
-#  cover           :string
-#
 require "test_helper"
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  attr_reader :course
+
+  def setup
+    @course = courses(:one)
+  end
+
+  test "when title and content is provided is valid" do
+    assert course.valid?
+  end
+
+  test "when title is missing is invalid" do
+    course.title = " "
+    assert_not course.valid?
+  end
+
+  test "when content is missing is invalid" do
+    course.content = " "
+    assert_not course.valid?
+  end
+
+  test "responds_to cover_path" do
+    assert_respond_to course, :cover_path
+  end
+
+  test "builds cover_path from cover" do
+    course.cover = "hello.jpg"
+    assert_equal "courses/hello.jpg", course.cover_path
+  end
+
+  test "id and title are set as ransackable attributes" do
+    assert_equal ["id", "title"], Course.ransackable_attributes
+  end
 end

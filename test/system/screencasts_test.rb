66e181849c750a3159682f2d4c70a373de27d33c
obaidashfaq20 <obaidashfaq20@yahoo.com>
2023-08-17 17:21:40 +0500

    Test added for searchable screencasts

--- test/system/screencasts_test.rb
+++ test/system/screencasts_test.rb
@@ -18,4 +18,36 @@ class ScreencastsTest < ApplicationSystemTestCase
     
     assert_selector 'h1', text: screencast.title
   end
+
+  test "should search Screencasts with search term" do
+    visit screencasts_url
+    assert_selector "h1", text: "Screencasts about Ruby and Ruby on Rails"
+
+    assert_selector "h3", text: screencast.title
+
+    serach_screencast_title(screencast.title)
+    assert_equal true, page.has_content?("Search Term: #{screencast.title}")
+
+    assert_selector "h3", text: screencast.title
+  end
+
+  test "should not show invalid results" do
+    visit screencasts_url
+    assert_selector "h1", text: "Screencasts about Ruby and Ruby on Rails"
+
+    assert_selector "h3", text: screencast.title
+
+    serach_screencast_title('invalid')
+    assert_equal true, page.has_content?("Search Term: invalid")
+
+    # Page should not have that screencast
+    refute_selector "h3", text: screencast.title
+  end
+
+  private
+
+  def serach_screencast_title(title)
+    fill_in 'search_term', with: title
+    click_button 'Search'
+  end
 end

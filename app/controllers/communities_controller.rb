673455b72d714b945ed3f7d0126f570502cc04ff
Lucian Ghinda <lucianghinda@users.noreply.github.com>
2023-09-13 10:53:13 +0300

    Ordered communities by title

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -2,13 +2,13 @@ class CommunitiesController < ApplicationController
 
   before_action :authenticate_admin!, only: %i[new edit create update destroy]
   before_action :set_community, only: %i[show edit update destroy]
-  
+
   layout "admin", only: %i[new edit]
 
   def index
     set_meta_tags title: "#{Community.count} communities about Ruby & Ruby on Rails", description: "The largest collection of communitys about Ruby & Ruby on Rails. Find community that will help you learn Ruby 3, Ruby on Rails 7, Hotwire, TurboFrame, and become a better programmer in general", keywords: 'Community, Ruby, Ruby 3, Ruby on Rails 7, Ruby on Rails 6, Hotwire, Turbo Frame, Stimulus, Vue with Rails, React with Rails, Tailwind with Rails, learn ruby, learn ruby on rails'
 
-    @communities = Community.all.order(created_at: :asc)
+    @communities = Community.all.order(title: :asc)
     render layout:"index_page"
   end
 
@@ -27,9 +27,9 @@ class CommunitiesController < ApplicationController
 
   def show
     @community = Community.friendly.find(params[:id])
-    @related = Community.where(id: Community.pluck(:id).sample(3)) 
+    @related = Community.where(id: Community.pluck(:id).sample(3))
 
-    @with_tags = "" 
+    @with_tags = ""
     @with_authors = ""
     @with_related = ""
     @with_lessons = ""
@@ -56,7 +56,7 @@ class CommunitiesController < ApplicationController
     @community.destroy
     redirect_to communities_path
   end
-  
+
   private
 
   def set_community
